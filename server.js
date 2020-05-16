4fbd9add16db4bbcf5e3e2557854fddd75002008
asedlack <asedlackcode@gmail.com>
2020-05-15 18:11:18 -0700

    login system added

--- server.js
+++ server.js
@@ -3,8 +3,18 @@ var PORT = process.env.PORT || 8000;
 var app = express();
 
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+var ExHb = require("express-handlebars");
+
+app.engine('handlebars', ExHb({ defaultLayout: 'main' }) )
+app.set('view engine', 'handlebars');
+
+
+var loginRoute = require("./routes/login");
+
+app.use(loginRoute);
 
 app.listen(PORT, function() {
     console.log("listening on port: http://localhost:" + PORT);
