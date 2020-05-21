e50ac67e8ed11a82486ec7eee3048c82e56d0b02
lucidCk <curtiskilgore@hotmail.com>
2020-05-20 18:57:46 -0700

    modified models and added index.js

--- models/login.js
+++ models/login.js
@@ -1,20 +1,32 @@
 // Sequelize (capital) references the standard library
 var Sequelize = require("sequelize");
 // sequelize (lowercase) references our connection to the DB.
-var sequelize = require("../config/connection.js");
+// var sequelize = require("../config/connection.js");
 
-// Creates a "Scores" model that matches up with DB
-var Login = sequelize.define("login", {
+// // Creates a "Scores" model that matches up with DB
+// var Login = sequelize.define("login", {
   
-    // the usereName gets saved as a string
-  userName: Sequelize.STRING,
+//     // the usereName gets saved as a string
+//   userName: Sequelize.STRING,
 
-  // the user score gets saved as an integer
-  password: Sequelize.STRING
-})
+//   // the user score gets saved as an integer
+//   password: Sequelize.STRING
+// })
 
-// Syncs with DB
-Login.sync();
+// // Syncs with DB
+// Login.sync();
 
-// Makes the Login model available for other files (will also create a table)
-module.exports = Login;
+// // Makes the Login model available for other files (will also create a table)
+// module.exports = Login;
+
+
+module.exports = function(sequelize, DataTypes) {
+  var Login = sequelize.define("Login", {
+    
+    userName: DataTypes.STRING,
+
+    password: DataTypes.STRING
+  });
+
+  return Login;
+};
